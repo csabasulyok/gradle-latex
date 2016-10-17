6d3f1f9efd453f750f484066962e5634d324badf
Csaba Sulyok <csaba.sulyok@gmail.com>
2016-10-17 20:03:03 +0300

    resolve #13

--- src/main/groovy/org/gradlelatex/LatexExtension.groovy
+++ src/main/groovy/org/gradlelatex/LatexExtension.groovy
@@ -9,7 +9,7 @@ import org.slf4j.LoggerFactory
  */
 class LatexExtension {
   Logger LOG = LoggerFactory.getLogger(LatexExtension)
-  
+
   /**
    * Final properties
    */
@@ -17,65 +17,69 @@ class LatexExtension {
   final File auxDir
   final LatexUtils utils
   protected LinkedHashMap<String, LatexObj> objs = [:]
-  
+
   /**
    * Configurable properties
    */
   boolean quiet = true
   boolean cleanTemp = false
-  
-  
+
+
   LatexExtension(Project p) {
     this.p = p
     this.utils = new LatexUtils(p)
-    
+
     // create auxiliary directory
     this.auxDir = p.file('.gradle/latex-temp')
     this.auxDir.mkdirs()
     LOG.info "Project $p using auxiliary directory $auxDir"
   }
-  
+
   void tex(String texName) {
     tex([tex: texName])
   }
-  
+
   void tex(Map args) {
     String name = args.tex.take(args.tex.lastIndexOf('.'))
     objs[name] = new LatexObj()
-    
+
     objs[name].name = name
     objs[name].tex = p.file(args.tex)
     objs[name].pdf = p.file(args.pdf ?: "${name}.pdf")
     objs[name].bib = args.bib ? p.file(args.bib) : null
     objs[name].dependsOn = args.dependsOn.collect { dep -> objs[dep.take(dep.lastIndexOf('.'))] }
     objs[name].aux = args.aux ? p.files(args.aux) : null
-    
+
     LOG.info "Added builder for TeX file $args.tex"
     addPdfLatexTask(objs[name])
     if (args.bib) {
       addBibTexTask(objs[name])
     }
   }
-  
+
   void addPdfLatexTask(LatexObj obj) {
     LOG.info "Dynamically adding task 'pdfLatex.${obj.name}'"
     PdfLatexTask pdfLatexTask = p.task("pdfLatex.${obj.name}", type: PdfLatexTask, overwrite: true)
     pdfLatexTask.setProps(obj)
+    obj.dependsOn.each { depObj ->
+      PdfLatexTask depPdfLatexTask = p.tasks["pdfLatex.${depObj.name}"]
+      pdfLatexTask.dependsOn depPdfLatexTask
+    }
     p.tasks["pdfLatex"].dependsOn "pdfLatex.${obj.name}"
-    
+
     LOG.info "Dynamically adding task 'cleanLatex.${obj.name}'"
     CleanLatexTask cleanLatexTask = p.task("cleanLatex.${obj.name}", type: CleanLatexTask, overwrite: true)
     cleanLatexTask.setProps(obj)
     p.tasks["cleanLatex"].dependsOn "cleanLatex.${obj.name}"
   }
-  
+
   void addBibTexTask(LatexObj obj) {
     LOG.info "Dynamically adding task 'bibTex.${obj.name}'"
     BibTexTask bibTexTask = p.task("bibTex.${obj.name}", type: BibTexTask, overwrite: true)
     bibTexTask.setProps(obj)
     p.tasks["pdfLatex.${obj.name}"].dependsOn "bibTex.${obj.name}"
   }
-  
+
   void with(String name, Closure closure) {
     objs[name].dependsOn.each { dependentObj ->
       with("${dependentObj.name}", closure)
